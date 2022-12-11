008e9cdae9d7624512cc0dcb16f668b442b98111
Martin Rodriguez <68254461+littlee5521@users.noreply.github.com>
2022-12-11 13:58:02 -0800

    Math functinality working object created to handle the multiple paths for the operations only multiplication works

--- js/script.js
+++ js/script.js
@@ -1,38 +1,69 @@
-const allButtons = document.querySelectorAll(".calc-button")
+const valueButtons = document.querySelectorAll(".value")
+const operatorButtons = document.querySelectorAll('.operator')
+const equalButton = document.querySelector('.equal')
 const output = document.querySelector('.calc-text')
-const buttonOpperatos = {
-    float: ".",
-    add: '+',
-    equal: '=',
-    zero: '0',
-    three: '3',
-    two: '2',
-    one:'1',
-    subtract: '-',
-    six: '6',
-    five: '5',
-    four: '4',
-    multiply: '*',
-    nine: '9',
-    eight: '8',
-    seven: '7',
-    divide: '/'
+const evaluateThis = [];
+const buttonNumbers = {
+    zero: 0,
+    three: 3,
+    two: 2,
+    one:1,
+    six: 6,
+    five: 5,
+    four: 4,
+    nine: 9,
+    eight: 8,
+    seven: 7,
+    float: '.'
 }
-let evaluateThis = ''
-allButtons.forEach(item =>{
-    if(item.id=="equal" || item.id =='clear') {
-        console.log('Hello')
-    }
-    else {
+const buttonOperators = {
+    divide: '/',
+    subtract: '-',
+    add: '+', 
+    multiply: '*',
+}
+let currentValue = ''
+equalButton.addEventListener('click', () =>{
+    evaluateThis.push(currentValue)
+    currentValue = ''
+    evaluation();
+})
+operatorButtons.forEach(item =>{
+    item.addEventListener('click', () => {
+        let property = item.id
+        evaluateThis.push(currentValue)
+        evaluateThis.push(buttonOperators[property])
+        currentValue = '';
+    })
+})
+valueButtons.forEach(item =>{
         item.addEventListener('click', () => {
             let property = item.id
-            evaluateThis += buttonOpperatos[property]
-            console.log(evaluateThis)
-            output.textContent = evaluateThis
+            currentValue += buttonNumbers[property]
+            output.textContent = currentValue
         })
+    })
+function evaluation () {
+    while (evaluateThis.includes('*') || evaluateThis.includes('/') ){
+        let op1 = evaluateThis.indexOf('*')
+        let op2 = evaluateThis.indexOf('/')
+        console.log(op1)
+        console.log(op2)
+        console.log('I ran')
+        if (op1!=-1&&op2==-1) {
+            console.log('Hello')
+            mathPath.multiply(op1)
+        }
     }
-} )
-let hi = '27'
-let bye = '28.99'
-let woah = "+"
-console.log(+hi + +bye)
+}
+
+const mathPath = {
+    multiply: function(op1) {
+        temp = evaluateThis[op1-1] * evaluateThis [op1+1]
+        console.log('I ran to')
+        for (let i =0; i <3; i ++){
+            evaluateThis.splice(op1-1)
+        }
+        output.textContent = temp
+    }
+}
